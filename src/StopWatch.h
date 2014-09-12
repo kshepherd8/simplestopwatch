ef2be3ddaa0c993854c91c92e201f8d7d3eb2bcc
Spencer Russell <spencer.f.russell@gmail.com>
2014-09-12 18:51:44 -0400

    adds missing files

---
+++ src/StopWatch.h
@@ -0,0 +1,69 @@
+/************************************************************************//**
+ *
+ * \file StopWatch.h
+ *
+ * \addtogroup StopWatch StopWatch
+ * \{
+ *
+ * \brief
+ *
+ * \note
+ *
+ * \author kjshepherd ()
+ * \date 2014-09-08
+ *
+ ****************************************************************************/
+
+#ifndef _STOPWATCH_H
+#define _STOPWATCH_H
+
+/****************************************************************************
+ *                              INCLUDE FILES                               *
+ ****************************************************************************/
+#include "CommonDefines.h"
+#include "SysTimer.h"
+#include "ClockDigit.h"
+
+/****************************************************************************
+ *                     EXPORTED TYPES and DEFINITIONS                       *
+ ****************************************************************************/
+typedef enum
+{
+    STOPWATCH_UNITIALIZED,
+    STOPWATCH_IDLE,
+    STOPWATCH_RUNNING    
+} StopwatchState;
+
+typedef struct StopWatchTime
+{
+    int hour_high;
+    int hour_low;
+    int min_high;
+    int min_low;
+    int sec_high;
+    int sec_low;
+    int tenth;
+    int hundredth;
+} StopWatchTime;
+
+typedef struct StopWatch
+{
+    void (*Start)(void);
+    void (*Stop)(void);
+    void (*Reset)(void);
+    StopWatchTime (*GetTime)(void);
+} StopWatch;
+
+/****************************************************************************
+ *                              EXPORTED DATA                               *
+ ****************************************************************************/
+
+/****************************************************************************
+ *                     EXPORTED FUNCTION DECLARATIONS                       *
+ ****************************************************************************/
+StopWatch * StopWatch_Init(SystemTimerDevice * tim);
+void StopWatch_RenderTime(void);
+
+#endif /* _STOPWATCH_H */
+
+/** \}*/
