836ed9a3dc51999bf8d2a7369b07e201147aaa3f
Spencer Russell <spencer.f.russell@gmail.com>
2014-09-20 20:57:10 -0400

    begins implementation of ncurses UI

--- src/WatchInterface.c
+++ src/WatchInterface.c
@@ -1,8 +1,8 @@
 /************************************************************************//**
  *
- * \file main.c
+ * \file WatchInterface.c
  *
- * \addtogroup main main
+ * \addtogroup WatchInterface WatchInterface
  * \{
  *
  * \brief
@@ -10,29 +10,18 @@
  * \note
  *
  * \author kjshepherd ()
- * \date 2014-09-09
+ * \date 2014-09-20
  *
  ****************************************************************************/
 
 /****************************************************************************
  *                              INCLUDE FILES                               *
  ****************************************************************************/
-#include "main.h"
-#include "CommonDefines.h"
-#include <stdlib.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <signal.h>
-#include <time.h>
-#include <ncurses.h>
-#include "StopWatch.h"
-#include "SysTimer.h"
+#include "WatchInterface.h"
 
 /****************************************************************************
  *                      PRIVATE TYPES and DEFINITIONS                       *
  ****************************************************************************/
-
-//error message
 
 /****************************************************************************
  *                              PRIVATE DATA                                *
@@ -45,68 +34,6 @@
 /****************************************************************************
  *                     PRIVATE FUNCTION DECLARATIONS                        *
  ****************************************************************************/
-void makeDisplay(void);
-
-//int MAIN(int argc, char *argv[])
-int MAIN(void)
-{
-    SystemTimerDevice * sysTimer = SystemTimer_Init();
-    StopWatch * stopWatch = StopWatch_Init(sysTimer);
-
-    char input = 5;
-
-    //stopWatch->Start();
-    initscr();
-    refresh();
-    //makeDisplay();
-
-
-    while(1)
-    {
-	scanf("%d", &input);
-	switch(input)
-	{
-	    case 0:
-		printf("Thanks for playin', brah! \n");
-		return 0;
-	    case 1:
-		stopWatch->Start();
-		//StopWatch_RenderTime();
-		//printf("\nOption -> ");
-		break;
-	    case 2:
-		stopWatch->Stop();
-		//StopWatch_RenderTime();
-		//printf("\nOption -> ");
-		break;
-	    case 3:
-		stopWatch->Reset();
-		//StopWatch_RenderTime();
-		//printf("\nOption -> ");
-		break;
-	    case 4:
-		StopWatch_RenderTime();
-		//printf("\nOption -> ");
-		break;
-	}
-	input = 5;
-    }
-
-    endwin();
-    return 0;
-}
-
-void makeDisplay(void)
-{
-    printw("Stopwatch Options \n");
-    printw("0 -> quit \n");
-    printw("1 -> start \n");
-    printw("2 -> stop \n");
-    printw("3 -> reset \n");
-    printw("4 -> print time \n");
-    printw("\nOption -> ");
-    refresh();
-}
 
 /****************************************************************************
  *                     EXPORTED FUNCTION DEFINITIONS                        *
--- test/test_WatchInterface.c
+++ test/test_WatchInterface.c
@@ -1,38 +1,25 @@
 /************************************************************************//**
  *
- * \file main.c
+ * \file test_WatchInterface.c
  *
- * \addtogroup main main
- * \{
- *
- * \brief
+ * \brief Test scripts for module WatchInterface to be run with Unity
  *
  * \note
  *
  * \author kjshepherd ()
- * \date 2014-09-09
+ * \date 2014-09-20
  *
  ****************************************************************************/
 
 /****************************************************************************
  *                              INCLUDE FILES                               *
  ****************************************************************************/
-#include "main.h"
-#include "CommonDefines.h"
-#include <stdlib.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <signal.h>
-#include <time.h>
-#include <ncurses.h>
-#include "StopWatch.h"
-#include "SysTimer.h"
+#include "unity.h"
+#include "WatchInterface.h"
 
 /****************************************************************************
  *                      PRIVATE TYPES and DEFINITIONS                       *
  ****************************************************************************/
-
-//error message
 
 /****************************************************************************
  *                              PRIVATE DATA                                *
@@ -45,72 +32,23 @@
 /****************************************************************************
  *                     PRIVATE FUNCTION DECLARATIONS                        *
  ****************************************************************************/
-void makeDisplay(void);
-
-//int MAIN(int argc, char *argv[])
-int MAIN(void)
-{
-    SystemTimerDevice * sysTimer = SystemTimer_Init();
-    StopWatch * stopWatch = StopWatch_Init(sysTimer);
-
-    char input = 5;
-
-    //stopWatch->Start();
-    initscr();
-    refresh();
-    //makeDisplay();
-
-
-    while(1)
-    {
-	scanf("%d", &input);
-	switch(input)
-	{
-	    case 0:
-		printf("Thanks for playin', brah! \n");
-		return 0;
-	    case 1:
-		stopWatch->Start();
-		//StopWatch_RenderTime();
-		//printf("\nOption -> ");
-		break;
-	    case 2:
-		stopWatch->Stop();
-		//StopWatch_RenderTime();
-		//printf("\nOption -> ");
-		break;
-	    case 3:
-		stopWatch->Reset();
-		//StopWatch_RenderTime();
-		//printf("\nOption -> ");
-		break;
-	    case 4:
-		StopWatch_RenderTime();
-		//printf("\nOption -> ");
-		break;
-	}
-	input = 5;
-    }
-
-    endwin();
-    return 0;
-}
-
-void makeDisplay(void)
-{
-    printw("Stopwatch Options \n");
-    printw("0 -> quit \n");
-    printw("1 -> start \n");
-    printw("2 -> stop \n");
-    printw("3 -> reset \n");
-    printw("4 -> print time \n");
-    printw("\nOption -> ");
-    refresh();
-}
 
 /****************************************************************************
  *                     EXPORTED FUNCTION DEFINITIONS                        *
  ****************************************************************************/
+
+void setUp(void)
+{
+}
+
+void tearDown(void)
+{
+}
+
+void testWatchInterfaceShouldDoSomething(void)
+{
+    TEST_FAIL_MESSAGE("WatchInterface has no tests!");
+}
 
 /****************************************************************************
  *                     PRIVATE FUNCTION DEFINITIONS                         *
@@ -122,4 +60,4 @@ void makeDisplay(void)
  * \return
  ****************************************************************************/
 
-/** \}*/
+// End of file.
